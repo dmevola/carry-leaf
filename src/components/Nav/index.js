dee2704fa3073a7e372a0bc7778b9a935d591e86
dmevola <dmevola@gmail.com>
2022-04-24 22:19:57 -0400

    nav tweaks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,10 +12,10 @@ function Navigation() {
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Collapse id ="responsive-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="/">About</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/resume">Resume</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="./">About</Nav.Link>
+            <Nav.Link href="./portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="./resume">Resume</Nav.Link>
+            <Nav.Link href="./contact">Contact</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Container>
