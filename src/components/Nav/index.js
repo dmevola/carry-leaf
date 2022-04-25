c25590f4d464c311846ea865a00fb63be97f918b
dmevola <dmevola@gmail.com>
2022-04-24 22:46:55 -0400

    still navving

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,14 +8,14 @@ function Navigation() {
     return(
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
             <Container>
-                <Navbar.Brand href="/carry-leaf/">Dan Evola</Navbar.Brand>
+                <Navbar.Brand href="/">Dan Evola</Navbar.Brand>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Collapse id ="responsive-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="./">About</Nav.Link>
-            <Nav.Link href="./portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="./resume">Resume</Nav.Link>
-            <Nav.Link href="./contact">Contact</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Container>
